4545342fe406f7febfe3fe3a10a99fb1566c0ed6
Azim <azimuddin807@gmail.com>
2024-12-26 16:55:06 +0600

    jobs details page

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const app = express()
 require('dotenv').config()
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(cors());
 app.use(express.json());
@@ -45,6 +45,13 @@ async function run() {
         const result = await cursor.toArray()
         res.send(result)
     } )
+
+    app.get('/jobs/:id' , async(req,res) => {
+      const id = req.params.id;
+      const query = {_id : new ObjectId(id)}
+      const result = await jobsCollections.findOne(query)
+      res.send(result)
+    })
 
 
 
